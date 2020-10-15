da7883ab07561cf9f40b70da66ab7375b86480f3
HarmonicTons <roncin.thomas.l.l@gmail.com>
2020-10-15 12:37:47 +0000

    clean state

--- src/components/FindRepo/FindRepo.stories.tsx
+++ src/components/FindRepo/FindRepo.stories.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Story, Meta } from "@storybook/react/types-6-0";
-import { FindRepo, FindRepoProps } from "./FindRepo";
+import { FindRepo } from "./FindRepo";
 
 export default {
   title: "FindRepo",
   component: FindRepo
 } as Meta;
 
-const Template: Story<FindRepoProps> = args => <FindRepo {...args} />;
+const Template: Story = args => <FindRepo {...args} />;
 
 export const Primary = Template.bind({});
--- src/components/FindRepo/FindRepo.tsx
+++ src/components/FindRepo/FindRepo.tsx
@@ -1,13 +1,8 @@
 import React, { useState, useEffect, useCallback } from "react";
-import Repository from "./Repository";
 import { useAsyncFn } from "react-use";
 import axios from "axios";
 import "@fortawesome/fontawesome-free/js/all.js";
 import * as _ from "lodash";
-
-export interface FindRepoProps {
-  debounceTimeInMs?: number;
-}
 
 export interface IRepository {
   id: number;
@@ -30,55 +25,10 @@ export interface GithubResponse {
   items: Array<IRepository>;
 }
 
-export const FindRepo: React.FC<FindRepoProps> = ({
-  debounceTimeInMs = 200
-}) => {
-  const [searchInput, setSearchInput] = useState("");
-  const [
-    { value: repositories, loading, error },
-    fetchRepositories
-  ] = useAsyncFn(async (searchInput: string) => {
-    if (searchInput === "") {
-      return [];
-    }
-    const response = await axios.get<GithubResponse>(
-      `https://api.github.com/search/repositories?q=${searchInput}&sort=stars&order=desc`
-    );
-    return response.data.items;
-  }, []);
-  const debouncedFetchRepositories = useCallback(
-    _.debounce(fetchRepositories, debounceTimeInMs),
-    [fetchRepositories]
-  );
-  useEffect(() => {
-    if (searchInput.length < 3) {
-      return;
-    }
-    debouncedFetchRepositories(searchInput);
-  }, [searchInput]);
+export const FindRepo: React.FC = () => {
   return (
     <div>
-      <label htmlFor="search">Search repository: </label>
-      <input
-        id="search"
-        value={searchInput}
-        onChange={e => setSearchInput(e.target.value)}
-      />
-      {loading && (
-        <div data-testid="spinner">
-          <i className="fas fa-circle-notch fa-spin"></i>
-        </div>
-      )}
-      {error && <div style={{ color: "red" }}>{error.message}</div>}
-      {!loading && (
-        <ul>
-          {repositories?.map(repo => (
-            <li key={repo.name}>
-              <Repository repository={repo} />
-            </li>
-          ))}
-        </ul>
-      )}
+      TODO
     </div>
   );
 };
