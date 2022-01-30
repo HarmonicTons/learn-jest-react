d80bcb0a5c481b1ca1c4cc090ec3cb9006ac3013
HarmonicTons <roncin.thomas.l.l@gmail.com>
2022-01-30 11:05:09 +0000

    half flow

--- src/components/CanvasLattice/CanvasLattice.stories.tsx
+++ src/components/CanvasLattice/CanvasLattice.stories.tsx
@@ -3,8 +3,16 @@ import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
 import { CanvasLattice } from "./CanvasLattice";
-import { getIndex, Lattice, makeLatticeAtEquilibrium } from "../../domain/lattice";
-import { Direction, Flags, getEquilibriumDistribution } from "../../domain/cell";
+import {
+  getIndex,
+  Lattice,
+  makeLatticeAtEquilibrium,
+} from "../../domain/lattice";
+import {
+  Direction,
+  Flags,
+  getEquilibriumDistribution,
+} from "../../domain/cell";
 import { PlotTypes } from "./render/render";
 import damJson from "./__testData__/dam.json";
 import { cloneDeep } from "lodash";
@@ -22,11 +30,7 @@ const Template: ComponentStory<typeof CanvasLattice> = args => (
  */
 const bump = makeLatticeAtEquilibrium(80, 32, 1, 0.1, 0);
 const barrierSize = 8;
-for (
-  let y = bump.y / 2 - barrierSize;
-  y <= bump.y / 2 + barrierSize;
-  y++
-) {
+for (let y = bump.y / 2 - barrierSize; y <= bump.y / 2 + barrierSize; y++) {
   const x = 20;
   bump.flag[x + y * bump.x] = Flags.barrier;
 }
@@ -36,6 +40,22 @@ Bump.args = {
   lattice: bump,
   gravity: 0,
   plotType: PlotTypes.rho,
+};
+
+/**
+ * CIRCULATING
+ */
+const circulating = makeLatticeAtEquilibrium(80, 32, 1, 0.1, 0);
+for (let y = 10; y <= 22; y++) {
+  const x = 20;
+  circulating.flag[x + y * bump.x] = Flags.source;
+}
+
+export const Circulating = Template.bind({});
+Circulating.args = {
+  lattice: circulating,
+  gravity: 0,
+  plotType: PlotTypes.curl,
 };
 
 /**
@@ -61,6 +81,38 @@ export const Flow = Template.bind({});
 Flow.args = {
   lattice: flow,
   gravity: 0,
+  viscosity: 0.02,
+  plotType: PlotTypes.curl,
+};
+
+/**
+ * FALF FLOW
+ */
+const halfFlow = makeLatticeAtEquilibrium(80, 32, 1, 0.1, 0);
+for (let x = 1; x < halfFlow.x - 1; x++) {
+  const y = 16;
+  halfFlow.flag[x + y * halfFlow.x] = Flags.interface;
+}
+for (let x = 1; x < halfFlow.x - 1; x++) {
+  for (let y = 17; y < halfFlow.y - 1; y++) {
+    halfFlow.flag[x + y * halfFlow.x] = Flags.gas;
+  }
+}
+
+for (let y = 8; y <= 16; y++) {
+  const x = 20;
+  halfFlow.flag[x + y * halfFlow.x] = Flags.barrier;
+}
+
+for (let y = 1; y < 16; y++) {
+  halfFlow.flag[1 + y * halfFlow.x] = Flags.source;
+  halfFlow.flag[halfFlow.x - 2 + y * halfFlow.x] = Flags.source;
+}
+
+export const HalfFlow = Template.bind({});
+HalfFlow.args = {
+  lattice: halfFlow,
+  gravity: 0.001,
   viscosity: 0.02,
   plotType: PlotTypes.curl,
 };
@@ -119,13 +171,16 @@ DamBreak.args = {
  */
 const communicatingVessels = cloneDeep(damJson) as Lattice;
 for (let y = 1; y < 5; y++) {
-  communicatingVessels.flag[getIndex(communicatingVessels.x, 30, y)] = Flags.interface;
+  communicatingVessels.flag[getIndex(communicatingVessels.x, 30, y)] =
+    Flags.interface;
 }
 for (let y = 1; y < 4; y++) {
-  communicatingVessels.flag[getIndex(communicatingVessels.x, 31, y)] = Flags.gas;
+  communicatingVessels.flag[getIndex(communicatingVessels.x, 31, y)] =
+    Flags.gas;
 }
 for (let y = 1; y < 16; y++) {
-  communicatingVessels.flag[getIndex(communicatingVessels.x, 50, y)] = Flags.barrier;
+  communicatingVessels.flag[getIndex(communicatingVessels.x, 50, y)] =
+    Flags.barrier;
 }
 export const CommunicatingVessels = Template.bind({});
 CommunicatingVessels.args = {
@@ -175,9 +230,17 @@ DamOverflow.args = {
  * SOURCE
  */
 const source = cloneDeep(damJson) as Lattice;
-const sd = getEquilibriumDistribution(1, 0.1, 0);
+const sd = getEquilibriumDistribution(1, 0.1, 0.1);
 for (let y = 1; y < 12; y++) {
   const i = getIndex(source.x, 1, y);
+  source.flag[i] = Flags.source;
+  Object.values(Direction).forEach(dir => {
+    source.distributions[dir][i] = sd[dir];
+    source.nextDistributions[dir][i] = sd[dir];
+  });
+}
+for (let y = 33; y < 44; y++) {
+  const i = getIndex(source.x, source.x - 2, y);
   source.flag[i] = Flags.source;
   Object.values(Direction).forEach(dir => {
     source.distributions[dir][i] = sd[dir];
